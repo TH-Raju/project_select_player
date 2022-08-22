02bdd433fefc4c54d1555ae830a7ce9e77a646f7
TH-Raju <rajukhan894200@gmail.com>
2022-08-22 22:57:38 +0600

    design done

---
+++ js/common.js
@@ -0,0 +1,4 @@
+document.getElementById('messi').addEventListener('click', function () {
+    const playerName = document.getElementsByClassName('player').textContent;
+    console.log(playerName);
+})
