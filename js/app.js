a62483a4b133e8f3d023875b555f973e08175322
TH-Raju <rajukhan894200@gmail.com>
2022-08-23 01:29:22 +0600

    player list add

---
+++ js/app.js
@@ -0,0 +1,6 @@
+addPlayer('select-messi', 'messi');
+addPlayer('select-neymar', 'neymar');
+addPlayer('select-pedri', 'pedri');
+addPlayer('select-ronaldinho', 'ronaldinho');
+addPlayer('select-ronaldo', 'ronaldo');
+addPlayer('select-sadio', 'sadio');
--- js/common.js
+++ js/common.js
@@ -1,4 +1,29 @@
-document.getElementById('messi').addEventListener('click', function () {
-    const playerName = document.getElementsByClassName('player').textContent;
-    console.log(playerName);
+//  this function is create for adding players into list 
+
+function addPlayer(selectName, plyerName) {
+    document.getElementById(selectName).addEventListener('click', function () {
+        const playersName = document.getElementById(plyerName).innerText;
+
+        const para = document.createElement("li");
+        const node = document.createTextNode(playersName);
+        para.appendChild(node);
+        const element = document.getElementById('player-show');
+        element.appendChild(para);
+
+        document.getElementById(selectName).disabled = true;
+
+    })
+}
+
+
+document.getElementById('calculate').addEventListener('click', function () {
+    const perPlayerIn = document.getElementById('perPlayer-field').value;
+    const perPlayer = parseFloat(perPlayerIn);
+    const expensesTotal = 5 * perPlayer;
+
+    const playerExpenses = document.getElementById('player-expenses');
+    playerExpenses.innerText = expensesTotal;
+    console.log(typeof (perPlayer));
+
+
 })
